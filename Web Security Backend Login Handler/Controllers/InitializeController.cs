691b58e9cf7a1b0594b7efc0a18235f9f76d76e3
Steven Orvis <steven@trappingpa.com>
2012-04-06 11:00:01 -0400

    Initial project

---
+++ Web Security Backend Login Handler/Controllers/InitializeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Security_Backend_Login_Handler.Controllers
+{
+    public class InitializeController : Controller
+    {
+        //
+        // GET: /Initialize/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Web Security Backend Login Handler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Security_Backend_Login_Handler.MvcApplication" Language="C#" %>
