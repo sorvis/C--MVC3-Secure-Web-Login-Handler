007cf77aede85be76770ac53ea0ff242ee056310
Steven Orvis <steven@trappingpa.com>
2012-04-06 11:04:13 -0400

    added authentication controller

---
+++ Web Security Backend Login Handler/Controllers/AuthenticationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_Security_Backend_Login_Handler.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        //
+        // GET: /Authentication/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //
+        // GET: /Analysis/initialize?key=fdlkgjlfjasf&
+
+        public ActionResult initialize(string key)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Analysis/authenticate?data=fdlkgjlfjasf&id=2354
+
+        public ActionResult initialize(string data, int id)
+        {
+            return View();
+        }
+
+    }
+}
