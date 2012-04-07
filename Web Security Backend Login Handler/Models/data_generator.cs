32c34392b9bc3417e15f1bd2310133abfe2fab25
Steven Orvis <steven@trappingpa.com>
2012-04-07 09:11:42 -0400

    Added protection in data_generator to protect against an infinent loop in the case of no unque data combinations left

--- Web Security Backend Login Handler/Models/data_generator.cs
+++ Web Security Backend Login Handler/Models/data_generator.cs
@@ -21,9 +21,17 @@ namespace Web_Security_Backend_Login_Handler.Models
         public static string get_random_data(IDataRepository db)
         {
             string random_data = getString();
+            int i = 0;
             while (!db.check_for_unique_data_string(random_data))
             {
                 random_data = getString();
+                i++;
+
+                // stop infinint loop incase we run out of possible login combinations
+                if (i > 100)
+                {
+                    return random_data;
+                }
             }
             return random_data;
         }
