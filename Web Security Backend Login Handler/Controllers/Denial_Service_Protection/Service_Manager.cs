d5ef51ef94def7f4ddd6a7d861ae00e06fd62369
Steven Orvis <steven@trappingpa.com>
2012-04-25 09:27:54 -0400

    Starting to add denial of service protection features

---
+++ Web Security Backend Login Handler/Controllers/Denial_Service_Protection/Service_Manager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Security_Backend_Login_Handler.Controllers.Denial_Service_Protection
+{
+    public class Service_Manager
+    {
+
+    }
+}
---
+++ Web Security Backend Login Handler/Controllers/Denial_Service_Protection/log_ip_fail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Security_Backend_Login_Handler.Controllers.Denial_Service_Protection
+{
+    public class log_ip_fail
+    {
+        public string IP_Address { get; set; }
+        public DateTime Last_Attempt { get; set; }
+        public int Fail_Count { get; set; }
+        public log_ip_fail(string ip)
+        {
+            IP_Address = ip;
+            Last_Attempt = DateTime.Now;
+            Fail_Count = 1; // This is the first instance of this ip so there is only one fail
+        }
+        public void Record_Failed_Attempt()
+        {
+            Last_Attempt = DateTime.Now;
+            Fail_Count++;
+        }
+        public int Seconds_Since_Last_Failed_Attempt()
+        {
+            return Last_Attempt.Second - DateTime.Now.Second;
+        }
+    }
+}
